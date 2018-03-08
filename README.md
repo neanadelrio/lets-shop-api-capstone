8959e18bcb70a9e7d4f348fda3c74f927ec79002
Neana Del Rio <sweetsunshyne@hotmail.com>
2018-03-08 18:43:00 -0500

    updated layout of app

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # lets-shop-api-capstone
 Thinkful api capstone app 
+Live Preview at https://neanadelrio.github.io/lets-shop-api-capstone/
+
+
+# Store-Search-Weather-API-Capstone
+Thinkful (https://www.thinkful.com) First Capstone Project - App integrating with Google and Openweather API
+
+![landing page] (https://github.com/neanadelrio/lets-shop-api-capstone/blob/master/Screen%20Shot%202018-03-08%20at%206.29.00%20PM.png)
+
+![overlay for googlemaps] ()
+
+![overlay for youtube] ()
+
+## Background
+I, like almost every fashion enthusiast hunts for the perfect store.  The stakes are even higher when in a new area and you’re not sure what stores are nearby.  I thought to create an app that retrieves not only the current weather for your excursion but most importantly nearby stores for your potential new coveted fashion piece.  
+
+## Use Case
+This app includes a location search input. A person can provide the current State or thier potential location to retrive current weather data along with the name and location of nearby stores from Google maps.
+
+## Working Prototype
+You can access a working prototype of the app here: https://neanadelrio.github.io/lets-shop-api-capstone/
+
+## Functionality
+The app's functionality includes:
+* The option to input your current or future location.
+* The app returns a google map indicating stores nearby or near targeted input location.
+* The app returns current weather from your location input.
+* A 'Go Back' button that takes you back to the main screen.
+
+## Technology
+* HTML
+* CSS
+* JavaScript
+* jQuery
+
+The app uses AJAX JSON calls to the <a href="https://maps.googleapis.com/maps">Google Maps</a>Open Platform API to return Store Location results.
+The app uses AJAX JSON calls to the <a href="https://api.openweathermap.org/data">Weather</a>Open Platform API to return Current Weather results.
+
+
+## Responsive
+App is built to be responsive across mobile, tablet, laptop, and desktop screen resolutions.
+
+## Development Roadmap
+This is v1.0 of the app, but future enhancements are expected to include:
--- app.js
+++ app.js
@@ -1,148 +1,133 @@
-const WEATHER_SEARCH_URL = "https://api.openweathermap.org/data/2.5/weather?id=524901&APPID=5b6db5278fb609ae944e0bd7f3f21c02";
+ const WEATHER_SEARCH_URL = "https://api.openweathermap.org/data/2.5/weather?id=524901&APPID=5b6db5278fb609ae944e0bd7f3f21c02";
 
-const GEOLOCATION_API = 'https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyAB8jz353KbMR6lRkCWCfpFBgHrlPQXUK8'
+// const GEOLOCATION_API = 'https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyAB8jz353KbMR6lRkCWCfpFBgHrlPQXUK8';
 
+"use strict";
  
- 
 
-  function initMap() {
-    var atlanta = {lat: 33.753746, lng: -84.386330};
-      map = new google.maps.Map(document.getElementById('map'), {
-          center: atlanta,
-          zoom: 15
+/* JQUERY CODES
+-------------------------------------------------*/
+
+$(document).ready(function() {
+
+    $.fn.animateRotate = function(angle, duration, easing, complete) {
+      let args = $.speed(duration, easing, complete);
+      let step = args.step;
+      return this.each(function(i, e) {
+        args.complete = $.proxy(args.complete, e);
+        args.step = function(now) {
+          $.style(e, 'transform', 'rotate(' + now + 'deg)');
+          if (step) return step.apply(e, arguments);
+        };
+
+        $({deg: 0}).animate({deg: angle}, args);
       });
-      
-
-    infowindow = new google.maps.InfoWindow();
-      var service = new google.maps.places.PlacesService(map);
-      service.nearbySearch({
-        location: atlanta,
-        radius: 500,
-        type: ['clothing_store']
-        }, callback);
-      }
-
-  function callback(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-        }
-      }
-  }
-      
-      
-
-  function createMarker(place) {
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
-      map: map,
-      position: place.geometry.location
+    };
+    
+    $("#main-page").css("height", "100vh");
+    $("#main-page").css("width", "100%");
+    $("#main-page").fadeIn();
+    $(".maincontent").fadeIn();
+    
+    $(".mainlink").on("click", function() {
+        $(".maincontent").fadeOut();
+        $("#main-page").animate({
+            width: "25px",
+            height: "375px"
+        }, function() {
+            $(this).animateRotate(90);
+        });
+        
+        setTimeout(function() {
+            $("#main-page").fadeOut();       
+        }, 1500);
+        
+        setTimeout(function() {
+            $("#next-page").animateRotate(0, 0);
+            $("#next-page").css("height", "25px");
+            $("#next-page").css("width", "375px");
+            $("#next-page").fadeIn();
+            $("#next-page").animate({
+      backgroundimage: "url(https://images.unsplash.com/photo-1485570661444-73b3f0ff9d2f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4550661e949789d265b9df2662108c93&auto=format&fit=crop&w=1663&q=80"
+            }, function() {
+                $(this).animate({
+                    height: "100vh"
+                }, function() {
+                    $(this).animate({
+                        width: "100%"
+                    }, function() {
+                        $(".nextcontent").fadeIn(300);
+                    });
+                });
+            });
+        }, 800);
     });
-
-    google.maps.event.addListener(marker, 'click', function() {
-      infowindow.setContent(place.name);
-      infowindow.open(map, this);
-      });
-    }
-
-//press on submit button and scroll to results
-function scrollPageTo(myTarget, topPadding) {
-    if (topPadding == undefined) {
-        topPadding = 0;
-    }
-    var moveTo = $(myTarget).offset().top - topPadding;
-    $('html, body').stop().animate({
-        scrollTop: moveTo
-    }, 200);
-}
+        
+    $(".nextlink").on("click", function() {
+        $(".nextcontent").fadeOut();
+        $("#next-page").animate({
+            width: "25px",
+            height: "375px"
+        }, function() {
+            $(this).animateRotate(-90);
+        });
+        
+        setTimeout(function() {
+            $("#next-page").fadeOut();          
+        }, 1500);
+        
+        setTimeout(function() {
+        $("#main-page").animateRotate(0, 0);
+        $("#main-page").css("height", "25px");
+        $("#main-page").css("width", "375px");
+            $("#main-page").fadeIn();
+            $("#main-page").animate({
+                height: "100vh"
+            }, function() {
+                $(this).animate({
+                    width: "100%"
+                }, function() {
+                    $(".maincontent").fadeIn(300);
+                });
+            });
+        }, 1400);
+    });
+    
+});
 
 //retrieve data from OpenWeather API
 function getWeatherData() {
-    let city = $('.search-query').val();
-    $.ajax(WEATHER_SEARCH_URL, {
-        data: {
-            units: 'imperial',
-            q: city
-        },
-        dataType: 'jsonp',
-        type: 'GET',
-        success: function (data) {
-            let widget = displayWeather(data);
-            $('#weather-display').html(widget);
-            scrollPageTo('#weather-display', 15);
-        }
-    });
+  let city = $('.search-query').val();
+  $.ajax(WEATHER_SEARCH_URL, {
+  data: {
+  units: 'imperial',
+  q: city
+  },
+    dataType: 'jsonp',
+    type: 'GET',
+    success: function (data) {
+    let widget = displayWeather(data);
+    $('#weather-display').html(widget);
+    scrollPageTo('#weather-display', 15);
+    }
+  });
 }
 
 function displayWeather(data) {
     return `
-    <div class="weather-results">
-        <h1><strong>Weather for ${data.name}</strong></h1>
+        <div class="weather-row">
+        <h3><strong>Current Weather for ${data.name}</strong></h3>
         <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
-        <p style="font-size:30px; margin-top:10px;">${data.weather[0].main}</p>
-        <p style="color:steelblue;" ">Description:</p><p"> ${data.weather[0].description}</p>
+        <p style="font-size:15px;">${data.weather[0].main}</p>
+        <p style="color:steelblue;">Description:</p><p> ${data.weather[0].description}</p>
         <p style="color:steelblue;">Temperature:</p><p> ${data.main.temp} &#8457; / ${(((data.main.temp)-32)*(5/9)).toFixed(2)} &#8451;</p>
         <p style="color:steelblue;">Humidity:</p><p> ${data.main.humidity} &#37;</p>
-    </div>
-`;
-}
-
-//retrieve data from FourSquare API
-function getFourSquareData() {
-  $('.category-button').click(function () {
-    let city = $('.search-query').val();
-    let category = $(this).text();
-    $.ajax(FOURSQUARE_SEARCH_URL, {
-      data: {
-        near: city,
-        venuePhotos: 1,
-        limit: 9,
-        query: 'recommended',
-        section: category,
-    },
-    dataType: 'json',
-      type: 'GET',
-      success: function (data) {
-      try {
-        let results = data.response.groups[0].items.map(function (item, index) {
-            return displayResults(item);
-        });
-        $('#foursquare-results').html(results);
-          scrollPageTo('#foursquare-results', 15);
-          } catch (e) {
-        $('#foursquare-results').html("<div class='result'><p>Sorry! No Results Found.</p></div>");
-        }
-        },
-        error: function () {
-        $('#foursquare-results').html("<div class='result'><p>Sorry! No Results Found.</p></div>");
-            }
-        });
-    });
-}
-
-function displayResults(result) {
-    return `
-        <div class="result col-3">
-            <div class="result-image" style="background-image: url(https://igx.4sqi.net/img/general/width960${result.venue.photos.groups[0].items[0].suffix})" ;>
-            </div>
-            <div class="result-description">
-                <h2 class="result-name"><a href="${result.venue.url}" target="_blank">${result.venue.name}</a></h2>
-                <span class="icon">
-                    <img src="${result.venue.categories[0].icon.prefix}bg_32${result.venue.categories[0].icon.suffix}" alt="category-icon">
-                </span>
-                <span class="icon-text">
-                    ${result.venue.categories[0].name}
-                </span>
-                <p class="result-address">${result.venue.location.formattedAddress[0]}</p>
-                <p class="result-address">${result.venue.location.formattedAddress[1]}</p>
-                <p class="result-address">${result.venue.location.formattedAddress[2]}</p>
-            </div>
         </div>
 `;
 }
 
 function enterLocation() {
-    $('.category-button').click(function () {
+    $(".mainlink").click(function () {
         $('button').removeClass("selected");
         $(this).addClass("selected");
     });
@@ -151,22 +136,10 @@ function enterLocation() {
         event.preventDefault();
         $('.navigation').removeClass("hide");
         $('#weather-display').html("");
-        $('#foursquare-results').html("");
         getWeatherData();
-        getFourSquareData();
         $('button').removeClass("selected");
     });
 }
 
-//autocomplete location name in form
-function activatePlacesSearch() {
-    let options = {
-        types: ['(regions)']
-    };
-    let input = document.getElementById('search-term');
-    let autocomplete = new google.maps.places.Autocomplete(input, options);
-}
 
 $(enterLocation);
-
-
